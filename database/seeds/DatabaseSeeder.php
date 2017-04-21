466e23b84f2fc10ad0723970c4b4cf0f1c095b59
mnlcandelaria <mnl.candelaria@gmail.com>
2017-04-21 15:52:03 +0800

    final setup ~1h

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        $this->call('CountryTableSeeder');
+        $this->call('ProvincesTableSeeder');
+        $this->call('CitiesTableSeeder');
+        $this->call('BarangayTableSeeder');
+    }
+}
